9802f55401b2c4db2a8f20f5432849bd63a8d432
joerverson santos <joerverson.santos@gmail.com>
2018-10-07 22:26:24 -0300

    ajustado e funcionando a api

--- src/middlewares/00001-cors.js
+++ src/middlewares/00001-cors.js
@@ -1,6 +1,6 @@
 export default (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+  // res.setHeader("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 };
